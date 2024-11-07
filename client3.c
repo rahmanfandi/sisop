73e8dd277df6a03b8587f88f9a7844b8cfe7fb25
rahman <nfandii@gmail.com>
2024-11-07 15:39:02 +0700

    bolak-balik

--- client3.c
+++ client3.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define SERVER_ADDR "127.0.0.1"
 #define PORT 8080
@@ -14,19 +15,36 @@ int main() {
     struct sockaddr_in server_addr;
     char buffer[BUFFER_SIZE];
 
+    // Membuat socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
     inet_pton(AF_INET, SERVER_ADDR, &server_addr.sin_addr);
 
-    connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    // Menghubungkan ke server
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection failed");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
     printf("Client connected with file descriptor: %d\n", sock);
 
-    send(sock, "Hello from client", 17, 0);
-    printf("Sent data to server.\n");
+    // Mengirim data ke server
+    const char *message = "Hello from client";
+    send(sock, message, strlen(message), 0);
+    printf("Sent data to server: %s\n", message);
 
-    recv(sock, buffer, sizeof(buffer), 0);
-    printf("Received from server: %s\n", buffer);
+    // Menerima respons dari server
+    int bytes_received = recv(sock, buffer, BUFFER_SIZE - 1, 0);
+    if (bytes_received > 0) {
+        buffer[bytes_received] = '\0';  // Null-terminate string
+        printf("Received response from server: %s\n", buffer);
+    }
 
     close(sock);
     return 0;
--- server3.c
+++ server3.c
@@ -3,55 +3,98 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <sys/select.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <poll.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
+#define MAX_CLIENTS 10  // Maksimum jumlah client yang bisa dilayani
 
 int main() {
     int server_fd, client_fd;
     struct sockaddr_in server_addr;
-    fd_set read_fds;
     char buffer[BUFFER_SIZE];
+    struct pollfd fds[MAX_CLIENTS];
+    int nfds = 1;  // Jumlah awal file descriptor (hanya server socket)
 
-    // Membuat socket
+    // Membuat socket server
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Konfigurasi alamat server
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
-    bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    listen(server_fd, 1);
 
-    FD_ZERO(&read_fds);
-    FD_SET(server_fd, &read_fds);
+    // Bind dan listen pada port
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, MAX_CLIENTS) < 0) {
+        perror("Listen failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Menambahkan server_fd ke poll set
+    fds[0].fd = server_fd;
+    fds[0].events = POLLIN;  // Memantau untuk koneksi masuk
 
     printf("Server listening on port %d...\n", PORT);
 
     while (1) {
-        fd_set temp_fds = read_fds;
-        select(server_fd + 1, &temp_fds, NULL, NULL, NULL);
-
-        if (FD_ISSET(server_fd, &temp_fds)) {
-            client_fd = accept(server_fd, NULL, NULL);
-            FD_SET(client_fd, &read_fds);
-            printf("New client connected with file descriptor: %d\n", client_fd);
+        // Panggil poll() untuk memonitor event pada file descriptor
+        int poll_count = poll(fds, nfds, -1);
+        if (poll_count < 0) {
+            perror("Poll failed");
+            exit(EXIT_FAILURE);
         }
 
-        for (int i = 0; i <= server_fd; i++) {
-            if (FD_ISSET(i, &temp_fds) && i != server_fd) {
-                int bytes_received = recv(i, buffer, sizeof(buffer), 0);
-                if (bytes_received <= 0) {
-                    printf("Closing connection with file descriptor: %d\n", i);
-                    close(i);
-                    FD_CLR(i, &read_fds);
+        // Loop melalui file descriptor yang aktif
+        for (int i = 0; i < nfds; i++) {
+            if (fds[i].revents & POLLIN) {
+                // Jika ada koneksi baru pada server socket
+                if (fds[i].fd == server_fd) {
+                    client_fd = accept(server_fd, NULL, NULL);
+                    if (client_fd < 0) {
+                        perror("Accept failed");
+                        continue;
+                    }
+                    printf("New client connected with file descriptor: %d\n", client_fd);
+
+                    // Tambahkan client baru ke poll set
+                    fds[nfds].fd = client_fd;
+                    fds[nfds].events = POLLIN;
+                    nfds++;
                 } else {
-                    printf("Received data from file descriptor %d: %s\n", i, buffer);
-                    send(i, "Hello", 5, 0);
+                    // Menerima data dari client yang ada
+                    int bytes_received = recv(fds[i].fd, buffer, BUFFER_SIZE - 1, 0);
+                    if (bytes_received <= 0) {
+                        printf("Closing connection with file descriptor: %d\n", fds[i].fd);
+                        close(fds[i].fd);
+                        fds[i] = fds[nfds - 1];  // Menghapus fd dari array
+                        nfds--;
+                    } else {
+                        buffer[bytes_received] = '\0';  // Null-terminate string
+                        printf("Received data from file descriptor %d: %s\n", fds[i].fd, buffer);
+
+                        // Mengirim respons ke client
+                        const char *response = "Hello from server";
+                        send(fds[i].fd, response, strlen(response), 0);
+                        printf("Sent response to file descriptor %d: %s\n", fds[i].fd, response);
+                    }
                 }
             }
         }
     }
 
+    close(server_fd);
     return 0;
 }
