14a6a91b8e53704cf52ca628103a6cb7142aec94
rahman <nfandii@gmail.com>
2024-11-11 10:21:03 +0700

    belajar 3

--- client3.c
+++ client3.c
@@ -1,49 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
-#define SERVER_ADDR "127.0.0.1"
 #define PORT 8080
-#define BUFFER_SIZE 1024
 
 int main() {
-    int sock;
-    struct sockaddr_in server_addr;
-    char buffer[BUFFER_SIZE];
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    char message[1024];
 
-    // Membuat socket
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Socket gagal\n");
+        return -1;
     }
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    inet_pton(AF_INET, SERVER_ADDR, &server_addr.sin_addr);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
 
-    // Menghubungkan ke server
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Connection failed");
-        close(sock);
-        exit(EXIT_FAILURE);
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("Alamat tidak valid\n");
+        return -1;
     }
-    printf("Client connected with file descriptor: %d\n", sock);
 
-    // Mengirim data ke server
-    const char *message = "Hello from client";
-    send(sock, message, strlen(message), 0);
-    printf("Sent data to server: %s\n", message);
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("Koneksi gagal\n");
+        return -1;
+    }
 
-    // Menerima respons dari server
-    int bytes_received = recv(sock, buffer, BUFFER_SIZE - 1, 0);
-    if (bytes_received > 0) {
-        buffer[bytes_received] = '\0';  // Null-terminate string
-        printf("Received response from server: %s\n", buffer);
+    while (1) {
+        printf("Masukkan perintah (POST/GET): ");
+        fgets(message, sizeof(message), stdin);
+
+        send(sock, message, strlen(message), 0);
+
+        valread = read(sock, buffer, sizeof(buffer));
+        buffer[valread] = '\0';
+        printf("%s", buffer);
+
+        memset(buffer, 0, sizeof(buffer)); // Bersihkan buffer
     }
 
     close(sock);
--- server3.c
+++ server3.c
@@ -1,100 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <poll.h>
+#include <unistd.h>
 
 #define PORT 8080
-#define BUFFER_SIZE 1024
-#define MAX_CLIENTS 10  // Maksimum jumlah client yang bisa dilayani
+#define MAX_NUMBERS 100
+
+int numbers[MAX_NUMBERS];
+int count = 0;
+
+void handle_client(int new_socket) {
+    char buffer[1024] = {0};
+    int valread;
+
+    while ((valread = read(new_socket, buffer, sizeof(buffer))) > 0) {
+        buffer[valread] = '\0';
+
+        if (strncmp(buffer, "POST", 4) == 0) {
+            int num;
+            sscanf(buffer + 5, "%d", &num); // Ambil angka setelah 'POST '
+
+            if (count < MAX_NUMBERS) {
+                numbers[count++] = num;
+                send(new_socket, "Penambahan angka berhasil\n", strlen("Penambahan angka berhasil\n"), 0);
+            } else {
+                send(new_socket, "Storage penuh\n", strlen("Storage penuh\n"), 0);
+            }
+
+        } else if (strncmp(buffer, "GET", 3) == 0) {
+            char response[1024] = {0};
+            for (int i = 0; i < count; i++) {
+                char num_str[12];
+                sprintf(num_str, "%d ", numbers[i]);
+                strcat(response, num_str);
+            }
+            strcat(response, "\n");
+            send(new_socket, response, strlen(response), 0);
+
+        } else {
+            send(new_socket, "Bad request\n", strlen("Bad request\n"), 0);
+        }
+
+        memset(buffer, 0, sizeof(buffer)); // Bersihkan buffer
+    }
+
+    close(new_socket);
+}
 
 int main() {
-    int server_fd, client_fd;
-    struct sockaddr_in server_addr;
-    char buffer[BUFFER_SIZE];
-    struct pollfd fds[MAX_CLIENTS];
-    int nfds = 1;  // Jumlah awal file descriptor (hanya server socket)
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
 
-    // Membuat socket server
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
-        perror("Socket creation failed");
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket gagal");
         exit(EXIT_FAILURE);
     }
 
-    // Konfigurasi alamat server
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
 
-    // Bind dan listen pada port
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Bind failed");
-        close(server_fd);
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind gagal");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_fd, MAX_CLIENTS) < 0) {
-        perror("Listen failed");
-        close(server_fd);
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen gagal");
         exit(EXIT_FAILURE);
     }
 
-    // Menambahkan server_fd ke poll set
-    fds[0].fd = server_fd;
-    fds[0].events = POLLIN;  // Memantau untuk koneksi masuk
-
-    printf("Server listening on port %d...\n", PORT);
+    printf("Server berjalan di port %d\n", PORT);
 
     while (1) {
-        // Panggil poll() untuk memonitor event pada file descriptor
-        int poll_count = poll(fds, nfds, -1);
-        if (poll_count < 0) {
-            perror("Poll failed");
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+            perror("Accept gagal");
             exit(EXIT_FAILURE);
         }
 
-        // Loop melalui file descriptor yang aktif
-        for (int i = 0; i < nfds; i++) {
-            if (fds[i].revents & POLLIN) {
-                // Jika ada koneksi baru pada server socket
-                if (fds[i].fd == server_fd) {
-                    client_fd = accept(server_fd, NULL, NULL);
-                    if (client_fd < 0) {
-                        perror("Accept failed");
-                        continue;
-                    }
-                    printf("New client connected with file descriptor: %d\n", client_fd);
-
-                    // Tambahkan client baru ke poll set
-                    fds[nfds].fd = client_fd;
-                    fds[nfds].events = POLLIN;
-                    nfds++;
-                } else {
-                    // Menerima data dari client yang ada
-                    int bytes_received = recv(fds[i].fd, buffer, BUFFER_SIZE - 1, 0);
-                    if (bytes_received <= 0) {
-                        printf("Closing connection with file descriptor: %d\n", fds[i].fd);
-                        close(fds[i].fd);
-                        fds[i] = fds[nfds - 1];  // Menghapus fd dari array
-                        nfds--;
-                    } else {
-                        buffer[bytes_received] = '\0';  // Null-terminate string
-                        printf("Received data from file descriptor %d: %s\n", fds[i].fd, buffer);
-
-                        // Mengirim respons ke client
-                        const char *response = "Hello from server";
-                        send(fds[i].fd, response, strlen(response), 0);
-                        printf("Sent response to file descriptor %d: %s\n", fds[i].fd, response);
-                    }
-                }
-            }
-        }
+        handle_client(new_socket);
     }
 
-    close(server_fd);
     return 0;
 }
